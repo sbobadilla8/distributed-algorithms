5b2f3852d0453fbd6c0ad03b2118989f5bfff892
Suhrid Gupta <s.gupta@allmediadesk.com>
2023-05-03 00:42:18 +1000

    So close ...

--- client/backend/mutex/mutex.py
+++ client/backend/mutex/mutex.py
@@ -1,13 +1,16 @@
 from threading import Thread
+import pickle as rick
+# import random, string
 
+# def randomword(length):
+#    letters = string.ascii_lowercase
+#    return ''.join(random.choice(letters) for i in range(length))
 
 class HemlockThread:
     def __init__(self, target, args):
-        self.grant = None
+        self.grant_value = None
+        # self.id = randomword(5)
         self.thread = Thread(target=target, args=args)
-
-    # def swap(self, tail):
-    #     self.grant = tail
 
     def get_thread(self):
         return self.thread
@@ -15,35 +18,64 @@ class HemlockThread:
 
 class Lock:
     def __init__(self):
-        self.value = 1
+         # Initial value: 0
         self.tail = None
 
-    def swap(self, thread):
-        if self.tail is None:
-            self.tail = thread
-            return 0
-        else:
-            prev = self.tail
-            self.tail = thread
-            return prev
+    # def swap(self, thread):
+    #     if self.tail is None:
+    #         self.tail = thread
+    #         return 0
+    #     else:
+    #         prev = self.tail
+    #         self.tail = thread
+    #         return prev
 
 
-def lock(l1, thread):
-    # l1.tail = thread.swap(l1.tail)
-    thread.grant = l1.swap(thread)
-    if thread.grant == 0:
+def lock(l1, hemlock_thread, hemlock_counter):
+    # lock is currently empty, append thread to it
+    print(f'{hemlock_counter} entering lock, value of l1 grant_value is {l1.tail}')
+    atomic_if = (hemlock_thread, 0)
+    atomic_else = (l1.tail, hemlock_thread)
+    if(l1.tail is None):
+        # l1.tail = hemlock_thread
+        # hemlock_thread.grant_value = 0
+        l1.tail, hemlock_thread.grant_value = atomic_if
+        print(f'l1 is empty, {hemlock_counter} is appending to l1')
         return
-    while thread.grant != 0:
-        if thread.grant == 0:
-            break
-            # thread.thread.start()  # access critical section
+    # lock is currently occupied by other thread, insert thread after lock
+    else:
+        hemlock_thread.grant_value, l1.tail = atomic_else
+        print(f'l1 is busy, {hemlock_counter} is appending to l1 and waiting.')
+    # read the grant_value value of its predecessor
+    next_grant_value = hemlock_thread.grant_value.grant_value
+    while(next_grant_value != l1):
+        next_grant_value = hemlock_thread.grant_value.grant_value
+        print(f'{hemlock_counter} successor grant_value: {next_grant_value}')
+    # Once neighbours grant_value value is l1, it means successor has completed critical section update.
+    # Update neighbours grant_value value to 0
+    print(f'{hemlock_counter} updating predecessor grant_value to 0')
+    hemlock_thread.grant_value.grant_value = 0
+    # Set its own grant_value value to 0 and return from lock to access critical section
+    print(f'{hemlock_counter} updating own grant_value to 0')
+    hemlock_thread.grant_value = 0
     return
-    # thread.grant = 0
-    # unlock(l1, thread)
 
 
-def unlock(l1, thread):
-    thread.grant = 0
-    if l1.tail == thread:
-        l1.tail = None  # lock has been completely released
-
+def unlock(l1, hemlock_thread, hemlock_counter, iterator):
+    # Hemlock thread sets its own grant_value to l1
+    print(f'{hemlock_counter} updating own grant_value to l1')
+    hemlock_thread.grant_value = l1
+    # Wait until its grant_value value gets reset to 0, or it is the only value in the lock
+    print(f'{hemlock_counter} waiting for own grant_value to update to 0')
+    
+    # TODO: Remove this later
+    if(iterator == 9):
+        print(f'Last iterator call')
+    print(f'In unlock, checking {l1} and {l1.tail} and {hemlock_thread.grant_value}')
+    while(hemlock_thread.grant_value != 0):
+        # print(f'In unlock, checking {l1.tail.id} and {hemlock_thread.id}')
+        # print(f'CAS result: {l1.tail.grant_value == l1}')
+        continue
+    # Once the value is reset to 0, it means its successor has acknowledged the unlock method, return
+    print(f'{hemlock_counter} own grant_value updated to 0, returning')
+    return
--- client/backend/mutex/test.py
+++ client/backend/mutex/test.py
@@ -5,30 +5,42 @@ from time import sleep, time
 l1 = Lock()
 
 
-def increase(var, t):
+def increase(var, h):
     for i in range(10):
         # mutex.acquire()
-        lock(l1, threads[t])
+        print(f'Entering {i} lock for {h}')
+        lock(l1, hemlocks[h], h)
         temp = var['value']
         print(temp)
-        sleep(0.0001)
+        # sleep(1)
         var['value'] = temp + 1
-        unlock(l1, threads[t])
+        unlock(l1, hemlocks[h], h, i)
+        print(f'Exiting {i} unlock for {h}')
         # mutex.release()
 
 
 count = {"value": 0}
-threads = []
-t1 = HemlockThread(target=increase, args=[count, 0])
-t2 = HemlockThread(target=increase, args=[count, 1])
-threads.append(t1)
-threads.append(t2)
-thread1 = t1.get_thread()
-thread2 = t2.get_thread()
+hemlocks = []
+h1 = HemlockThread(target=increase, args=[count, 0])
+h2 = HemlockThread(target=increase, args=[count, 1])
+h3 = HemlockThread(target=increase, args=[count, 2])
+h4 = HemlockThread(target=increase, args=[count, 3])
+hemlocks.append(h1)
+hemlocks.append(h2)
+hemlocks.append(h3)
+hemlocks.append(h4)
+thread1 = h1.get_thread()
+thread2 = h2.get_thread()
+thread3 = h3.get_thread()
+thread4 = h4.get_thread()
 thread1.start()
 thread2.start()
+thread3.start()
+thread4.start()
 thread1.join()
 thread2.join()
+thread3.join()
+thread4.join()
 # t1 = Thread(target=increase, args=[count])
 # t2 = Thread(target=increase, args=[count])
 # t1.start()
