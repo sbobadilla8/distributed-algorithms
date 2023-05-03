8ffe5b4de8f0ecf0c6940db29f67de02ae835ddd
Sebastian Bobadilla <seb.bobadilla8@gmail.com>
2023-05-03 10:28:22 +1000

    Added requirements.txt and formatted mutex.py

--- client/backend/mutex/mutex.py
+++ client/backend/mutex/mutex.py
@@ -1,5 +1,6 @@
 from threading import Thread
 
+
 class HemlockThread:
     def __init__(self, target, args):
         self.grant_value = None
@@ -11,7 +12,7 @@ class HemlockThread:
 
 class Lock:
     def __init__(self):
-         # Initial value: None
+        # Initial value: None
         self.tail = None
 
 
@@ -20,7 +21,7 @@ def lock(l1, hemlock_thread):
     # print(f'{hemlock_counter} entering lock, value of l1 grant_value is {l1.tail}')
     atomic_if = (hemlock_thread, 0)
     atomic_else = (l1.tail, hemlock_thread)
-    if(l1.tail is None):
+    if l1.tail is None:
         # l1.tail = hemlock_thread
         # hemlock_thread.grant_value = 0
         l1.tail, hemlock_thread.grant_value = atomic_if
@@ -32,7 +33,7 @@ def lock(l1, hemlock_thread):
         # print(f'l1 is busy, {hemlock_counter} is appending to l1 and waiting.')
     # read the grant_value value of its predecessor
     next_grant_value = hemlock_thread.grant_value.grant_value
-    while(next_grant_value != l1):
+    while next_grant_value != l1:
         next_grant_value = hemlock_thread.grant_value.grant_value
         # print(f'{hemlock_counter} successor grant_value: {next_grant_value}')
     # Once neighbours grant_value value is l1, it means successor has completed critical section update.
@@ -51,11 +52,11 @@ def unlock(l1, hemlock_thread):
     hemlock_thread.grant_value = l1
     # Wait until its grant_value value gets reset to 0, or it is the only value in the lock
     # print(f'{hemlock_counter} waiting for own grant_value to update to 0')
-    
-    while(hemlock_thread.grant_value != 0):
+
+    while hemlock_thread.grant_value != 0:
         # If the lock tail address is equal to the hemlock thread, this is the final thread in the queue
         # Set lock tail to None and stop waiting
-        if(l1.tail == hemlock_thread):
+        if l1.tail == hemlock_thread:
             l1.tail = None
             break
         continue
